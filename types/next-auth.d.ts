08a389b3ccc17dcaf029603ba9485a9b0ed7a089
afuhflynn <flyinnsafuh@gmail.com>
2025-05-21 01:12:10 +0100

    Fix build errors with auth.js left over

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,27 +1,27 @@
-import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
-import { JWT as DefaultJWT } from "better-auth/";
+// import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+// import { JWT as DefaultJWT } from "better-auth/";
 
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      username: string;
-      bio: string | null;
-      signedInAt: string;
-    } & DefaultSession["user"];
-  }
+// declare module "next-auth" {
+//   interface Session extends DefaultSession {
+//     user: {
+//       id: string;
+//       username: string;
+//       bio: string | null;
+//       signedInAt: string;
+//     } & DefaultSession["user"];
+//   }
 
-  interface User extends DefaultUser {
-    username: string;
-    bio: string | null;
-  }
-}
+//   interface User extends DefaultUser {
+//     username: string;
+//     bio: string | null;
+//   }
+// }
 
-declare module "next-auth/jwt" {
-  interface JWT extends DefaultJWT {
-    id: string;
-    username: string;
-    bio: string | null;
-    signedInAt: string;
-  }
-}
+// declare module "next-auth/jwt" {
+//   interface JWT extends DefaultJWT {
+//     id: string;
+//     username: string;
+//     bio: string | null;
+//     signedInAt: string;
+//   }
+// }
