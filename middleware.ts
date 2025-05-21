03170a9f7ce1031932994a1b3b542a350ba9045c
afuhflynn <flyinnsafuh@gmail.com>
2025-05-21 02:12:16 +0100

    fix nextjs middleware routing problem

--- middleware.ts
+++ middleware.ts
@@ -1,49 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
 
-export async function middleware(req: NextRequest) {
-  const cookies = req.cookies;
-  const authCookie = cookies.get("better-auth.session_token");
-  const { pathname } = req.nextUrl;
+export async function middleware(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request);
 
-  // Check if the user is authenticated
-  const isAuthenticated = authCookie !== undefined;
-
-  // Redirect authenticated users to the dashboard if they try to access the sign-up page
-  if (isAuthenticated && pathname === "/") {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
-
-  // If the user is not authenticated and trying to access a protected route, redirect to login
-  if (
-    !isAuthenticated &&
-    (pathname.startsWith("/dashboard") || pathname.startsWith("/chat"))
-  ) {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
-  }
-
-  if ((isAuthenticated || !isAuthenticated) && pathname === "/practice") {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
-  }
-  // If the user is authenticated and trying to access the login page, redirect to dashboard
-  if (isAuthenticated && pathname === "/sign-in") {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
-
-  // If the user is not authenticated and trying to access the signout page, redirect to home page
-  if (!isAuthenticated && pathname === "/sign-out") {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
-  // If the user is authenticated and trying to access the login page, redirect to dashboard
-  if (isAuthenticated && pathname.endsWith("/lessons")) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
   return NextResponse.next();
 }
 
-// general matcher regex array
-
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/dashboard"], // Specify the routes the middleware applies to
 };
